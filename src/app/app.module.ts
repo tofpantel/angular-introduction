b20b898ecd3524c796dc29d0b745bdb2ca7c0a0a
Pantelis Papastergiou <pantpapastergiou@panteliss-mbp.station>
2023-04-13 18:17:21 +0300

    add event and one-way binds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { EventBindComponent } from './event-bind/event-bind.component';
+import { OneWayBindComponent } from './one-way-bind/one-way-bind.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GreetingComponent
+    GreetingComponent,
+    EventBindComponent,
+    OneWayBindComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/one-way-bind/one-way-bind.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-way-bind',
+  templateUrl: './one-way-bind.component.html',
+  styleUrls: ['./one-way-bind.component.css']
+})
+export class OneWayBindComponent {
+
+}
